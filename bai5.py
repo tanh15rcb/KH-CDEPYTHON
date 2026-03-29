52fbaaf5dacdc005bbfcf08bf15e1d7a58193928
tanh15rcb <anh0972681@gmail.com>
2026-03-29 21:23:34 +0700

    Add files via upload

---
+++ bai5.py
@@ -0,0 +1,22 @@
+import math
+
+a = float(input("Nhập a: "))
+b = float(input("Nhập b: "))
+c = float(input("Nhập c: "))
+
+if a == 0:
+    print("Đây không phải phương trình bậc 2")
+else:
+    delta = b**2 - 4*a*c
+    
+    if delta < 0:
+        print("Phương trình vô nghiệm")
+    elif delta == 0:
+        x = -b / (2*a)
+        print("Phương trình có nghiệm kép: x =", x)
+    else:
+        x1 = (-b + math.sqrt(delta)) / (2*a)
+        x2 = (-b - math.sqrt(delta)) / (2*a)
+        print("Phương trình có 2 nghiệm:")
+        print("x1 =", x1)
+        print("x2 =", x2)
